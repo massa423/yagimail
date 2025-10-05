9298e342571cace5c91c70b87b364031895d6f54
Suzuki Masaya <ikachang-kawaii@nifty.com>
2025-10-05 21:16:37 +0900

    chore: add linter and formatter

---
+++ frontend/prettier.config.mjs
@@ -0,0 +1,17 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  trailingComma: 'all',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  endOfLine: 'lf',
+  bracketSpacing: true,
+};
+
+export default config;
